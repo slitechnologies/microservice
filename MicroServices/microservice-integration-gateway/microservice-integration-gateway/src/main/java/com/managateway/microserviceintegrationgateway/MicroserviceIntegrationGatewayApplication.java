9e54e69ec33e1f64c86899ec1a65256dfc61fb83
Tinodashe kayenie <90652443+slitechnologies@users.noreply.github.com>
2024-03-13 16:45:30 +0200

    Gateway added

---
+++ MicroServices/microservice-integration-gateway/microservice-integration-gateway/src/main/java/com/managateway/microserviceintegrationgateway/MicroserviceIntegrationGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.managateway.microserviceintegrationgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceIntegrationGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceIntegrationGatewayApplication.class, args);
+	}
+
+}
---
+++ MicroServices/microservice-integration-gateway/microservice-integration-gateway/src/test/java/com/managateway/microserviceintegrationgateway/MicroserviceIntegrationGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managateway.microserviceintegrationgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceIntegrationGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
